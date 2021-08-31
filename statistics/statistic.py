da900640f5448e9fe3ec3023a8c87a3a94e64951
Hao-Jiun-Tu <tuj0419@gmail.com>
2021-08-31 23:34:02 +0800

    init commit

---
+++ statistics/statistic.py
@@ -0,0 +1,24 @@
+# STATISTICS
+import random
+data = random.choice([1, 5, 6, 10, 20])
+print(data)
+data = random.sample([1, 5, 6, 10, 20], 3)
+print(data)
+data = [1, 5, 8, 20]
+random.shuffle(data)           # change order randomly
+print(data)
+data = random.uniform(60, 100) # random number betwenn 60 to 100
+print(data)
+# normal distribution
+# mean = 100, std = 10
+data = random.normalvariate(100, 10)
+print(data)
+
+import statistics as stat
+data = stat.mean([1, 4, 5, 8])
+print(data)
+data = stat.median([1, 2, 3, 4, 5, 8, 100])
+print(data)
+data = stat.stdev([1, 2, 3, 4, 5, 8, 100])
+print(data)
+

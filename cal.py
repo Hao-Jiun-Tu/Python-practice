4757f899d22d2aa3e454d59603b42bc8b44cd41f
Hao-Jiun-Tu <tuj0419@gmail.com>
2021-08-31 16:29:48 +0800

    init commit

---
+++ cal.py
@@ -0,0 +1,23 @@
+# CALCULATIONS
+
+# DIV
+x = 10 / 4  # = 2.5
+print(x)
+x = 10 // 4 # = 2
+print(x)
+# POWER
+x = 2 ** 2
+print(x)
+# SQRT
+x = 2 ** 0.5
+print(x)
+
+# STRING CAL
+s = "Hello" * 3 + "World"
+print(s)
+s = """Hello
+World"""      # new line
+print(s)
+print(s[0:5]) # print Hello
+              # contain the beginning but not the ending
+
---
+++ data_type.py
@@ -0,0 +1,42 @@
+# DATA TYPE
+# NUMBER
+31415
+3.1415
+
+# STRINGS
+"Testing"
+'Testing'
+print("Testing")
+print('Testing')
+print("'Testing'")
+print("\"Testing\"")
+
+# BOOLEAN
+True
+False
+
+# LIST
+[3,4,5]
+["Hello", "World"]
+
+# TUPLE
+(3,4,5)
+("Hello", "World")
+
+# SET
+{3,4,5}
+{"Hello", "World"}
+
+# DICTIONARY
+{"apple" : "fruit", "python" : "program"}
+
+# DATA
+x = 3
+print(x)
+x = True
+print(x)
+x = "Hello"
+print(x)
+x = {3,4,5}
+print(x)
+

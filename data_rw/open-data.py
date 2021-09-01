a51a44770ef97a0ebcb8dcb8e013dd22ecb24b38
Hao-Jiun-Tu <tuj0419@gmail.com>
2021-09-01 12:28:39 +0800

    init commit

--- data_rw/open-data.py
+++ data_rw/open-data.py
@@ -1,6 +1,14 @@
 # GET OPEN-SRC DATA FROM NETWORK
 import urllib.request as request
 import json
+
+# access the source code from NTU network
+src = "http://www.ntu.edu.tw"
+with request.urlopen(src) as response:
+    data = response.read().decode("utf-8")
+print(data)
+
+# access the data in json format from network
 src = "https://data.taipei/api/v1/dataset/296acfa2-5d93-4706-ad58-e83cc951863c?scope=resourceAquire"
 with request.urlopen(src) as response:
     data = json.load(response)

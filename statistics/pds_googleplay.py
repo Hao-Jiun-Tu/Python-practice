e1eba85bcafb62ad4f5517a1debe5bd1e489213a
Hao-Jiun-Tu <tuj0419@gmail.com>
2021-09-02 20:07:26 +0800

    init commit

--- statistics/pds_googleplay.py
+++ statistics/pds_googleplay.py
@@ -8,3 +8,23 @@ print(data)
 print("Data-num:", data.shape)
 print("Data-col:", data.columns)
 print("=======================")
+
+# condition = (data["Rating"] > 5)
+# target = data[condition]
+# print(data)
+condition = (data["Rating"] <= 5)
+target = data[condition]
+print("Avg:", target["Rating"].mean())
+print("Median:", target["Rating"].median())
+print("The first hundred avg:", target["Rating"].nlargest(100).mean())
+
+# print(data["Installs"])
+print(data["Installs"][10472]) # print out "Free" --> garbage data
+data["Installs"] = pd.to_numeric(data["Installs"].str.replace("[,+]", "").replace("Free", ""))
+print(data["Installs"])
+condition = (data["Installs"] > 100000)
+print("Installing quantities larger than 1000000", data[condition].shape[0])
+
+keyword = input("Please enter the keyword: ")
+condition = data["App"].str.contains(keyword, case = False)
+print("The number of App containing keyword:", data[condition].shape[0])
